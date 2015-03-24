5e80a7cfb56cae54e4a7bd8b5b784752fb035fc3
jcskyting <jcskyting@gmail.com>
2015-03-24 16:46:26 +0800

    flow control ex5

--- flow_control/ex3.rb
+++ flow_control/ex3.rb
@@ -2,6 +2,20 @@ def numregion(num)
   (num>100)? "above 100" : ((num>50)? "51 to 100" : "0 to 50")
 end
 
-p numregion(90)
-p numregion(30)
-p numregion(1529)
+def evaluate(num)
+  case
+  when num < 0
+    puts "You can't enter a negative num!"
+  when num <= 50
+    puts "#{num} is between 0 and 50"
+  when num <= 100
+    puts "#{num} is between 51 and 100"
+  else
+    puts "#{num} is above 100"
+  end
+end
+
+
+evaluate(90)
+evaluate(30)
+evaluate(1529)
